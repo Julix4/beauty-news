886c375406d8be9c500cdbecd0fc42dba6a6d266
Julieth V <121585096+Julix4@users.noreply.github.com>
2025-01-03 22:26:20 +0100

    Create and fix create_articles and fix scrape_articles due to module errors.

---
+++ scripts/create_articles.py
@@ -0,0 +1,69 @@
+# fmt: off
+"""
+Run the following code to summarize news for creating new articles using the Gemini API.
+Use 'pip install -q google-generativeai' to install the required package.
+"""
+import json
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from scripts.scrape_articles import scrape_beauty_news
+import google.generativeai as genai
+
+
+# Function for summarizing news
+def summarize_news(articles):
+    # Use the API to create an article
+    genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+    print("after API key")
+
+    # Create the model
+    generation_config = {
+        "temperature": 1,
+        "top_p": 0.95,
+        "top_k": 40,
+        "max_output_tokens": 8192,
+        "response_mime_type": "text/plain",
+    }
+
+    model = genai.GenerativeModel(
+        model_name="gemini-2.0-flash-exp",
+        generation_config=generation_config,
+    )
+
+    chat_session = model.start_chat(history=[])
+
+    response = chat_session.send_message(
+        "With these instructions:\n- The article should be 700-1000 words.\n" +
+        "- Make sure to not add any brand or product name\n" +
+        "- Only relevant information from the text provided, useful for marketing and the beauty community.\n\n" +
+        "Summarize into one article for Medium the following articles:\n" + "\n".join([article for article in articles])
+    )
+
+    # Return the summarized news
+    print(response.text)
+
+# Function to convert response text to markdown format
+
+
+def to_markdown(text):
+    return f"### Summary\n\n{text}"
+
+
+# Example usage
+
+# Ruta al archivo JSON
+article_links_path = "data/article_links_example.json"
+
+# Leer y parsear el JSON
+with open(article_links_path, "r", encoding="utf-8") as file:
+    article_links = json.load(file)
+
+articles = []
+
+for i, article in enumerate(article_links, start=0):
+    article = scrape_beauty_news(article_links[i]["link"])
+    articles.append(article['text'])
+    print(f"{i}. {article['title']}\n   Text: {article['text']}\n")
+
+summarize_news(articles)
--- scripts/scrape_articles.py
+++ scripts/scrape_articles.py
@@ -1,15 +1,19 @@
+# fmt: off
+
 import json
-import data.keywords as keywords
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from goose3 import Goose
 # import scripts.fetch_beauty...
+import data.keywords as keywords
 from scripts.fetch_beauty_news import fetch_beauty_news
 from scripts.fetch_beauty_scholar import fetch_beauty_scholar
 
 # Scraping beauty news
 
 
-def summarize_beauty_news(url):
+def scrape_beauty_news(url):
     g = Goose()
     article = g.extract(url)
 
@@ -25,12 +29,12 @@ KEYWORDS = keywords.KEYWORDS["news"]
 articles = fetch_beauty_news(API_KEY, KEYWORDS) """
 
 # Ruta al archivo JSON
-articles_path = "data/articles_example.json"
+articles_path = "data/article_links_example.json"
 
 # Leer y parsear el JSON
 with open(articles_path, "r", encoding="utf-8") as file:
     articles = json.load(file)
 
 for i, article in enumerate(articles, start=0):
-    article = summarize_beauty_news(articles[i]["link"])
+    article = scrape_beauty_news(articles[i]["link"])
     print(f"{i}. {article['title']}\n   Text: {article['text']}\n")
